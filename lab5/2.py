88720cf0b176b51cdd04080684675dfb1a6a998e
Aruzhan <ar_satybaldy@kbtu.kz>
2023-03-03 21:58:18 +0600

    all exercises finished

---
+++ lab5/2.py
@@ -0,0 +1,9 @@
+import re
+# Write a Python program that matches a string that has an 'a' followed by two to three 'b'.
+str = "abbb"
+pattern2 = r"ab{2,3}"
+x = re.search(pattern2, str)
+if x: 
+    print("A match is found!")
+else:
+    print("Not matched!")
---
+++ lab5/3.py
@@ -0,0 +1,10 @@
+import re
+# Write a Python program to find sequences of lowercase letters joined with a underscore.
+str = "hk_jnnh"
+pattern3 = r"^[a-z]+_[a-z]+$"
+x = re.findall(pattern3, str)
+if x: 
+    print(x)
+    print("A match is found!")
+else:
+    print("Not matched!")
